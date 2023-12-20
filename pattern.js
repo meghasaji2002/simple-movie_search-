f02a5b29274c277777dee5c04591e143e27d28ec
Megha Saji <meghasaji2002@gmail.com>
2023-12-21 01:05:28 +0530

    v1

---
+++ pattern.js
@@ -0,0 +1,29 @@
+for(r=4;r>=2;r--){
+    str=''
+for(c=1;c<=7;c++){
+if(c+r==5 || c-r==3 || r==4){
+    str=str+' A'
+
+}
+
+else{
+    str=str+'  '
+ 
+}
+}
+
+console.log(str);
+}
+for(r=1;r<=4;r++){
+    str=''
+for(c=1;c<=7;c++){
+if(c+r==5 || c-r==3 || r==4){
+    str=str+' A'
+}
+
+else{
+    str=str+'  '   
+}
+}
+
+console.log(str); }
